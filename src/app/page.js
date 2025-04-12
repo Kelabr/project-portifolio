0c767a83676424d76b1883e07ecdf5bd5893a1a4
Kelabr <kelabrantes.me@gmail.com>
2025-04-12 18:03:51 -0300

    component creation of Exhibitor

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,10 @@
-import Card from "@/components/Card";
+import Exhibitor from "@/components/Exhibitor";
 
 
 export default function Home() {
   return (
     <main className="px-4">
-      <Card name="Projeto do Grafite" descriptionn="Um projeto dedicado ao grafite de rua com base na cidade de cubatão, visando tirar os meninos da boca e colocalos na arte" localy="Cubatão - SP"/>
+      <Exhibitor/>
     </main>
   );
 }
---
+++ src/components/Exhibitor.jsx
@@ -0,0 +1,9 @@
+import Card from "./Card";
+
+export default function Exhibitor(){
+    return (
+        <div>
+            <Card name="Projeto do Grafite" descriptionn="Um projeto dedicado ao grafite de rua com base na cidade de cubatão, visando tirar os meninos da boca e colocalos na arte" localy="Cubatão - SP"/>
+        </div>
+    )
+}
